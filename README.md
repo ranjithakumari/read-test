543ba79f01d3e821b0f75be6b623d2f5c13da719
ranjithakumari <54302203+ranjithakumari@users.noreply.github.com>
2022-07-29 14:33:34 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# read_test
+# littletiers
+little tiers
---
+++ http_server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app     = express();
+const low     = require('lowdb');
+const fs      = require('lowdb/adapters/FileSync');
+const adapter = new fs('db.json');
+const db      = low(adapter);
+const cors    = require('cors');
+
+// allow cross-origin resource sharing (CORS)
+app.use(cors());
+
+// data parser - used to parse post data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// serve static files from public directory
+// -------------------------------------------
+app.use(express.static('public'));
+
+// init the data store
+db.defaults({ users: []}).write();
+
+// return all users
+app.get('/data', function(req, res){     
+    res.send(db.get('users').value());
+});
+
+// add user
+app.post('/add', function(req, res){
+    var user = {
+        'name'         : req.body.name,
+       'email'        : req.body.email,
+    }
+    db.get('users').push({user}).write();
+    console.log(db.get('users').value());
+    res.send(db.get('users').value());
+});
+
+// start server
+// -----------------------
+app.listen(3000, function(){
+    console.log('Running on port 3000!')
+})
